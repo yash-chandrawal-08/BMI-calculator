cf42df53ad75f8d6269650cb71ceab8de88f0b54
yash chandrawal <yash.chandrawal8@gmail.com>
2024-10-05 13:48:38 +0530

    add splash screen and theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,4 +37,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation ("com.intuit.sdp:sdp-android:1.1.1")
 }
